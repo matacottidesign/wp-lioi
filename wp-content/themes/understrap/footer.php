9e58b8d906b01fdcf7dfd1eee2528219374b3fe1
Francesco Matacotti <francesco1984.go@gmail.com>
2020-01-13 17:43:58 +0100

    aggiunto footer + corretto script per i link da ACF

--- wp-content/themes/understrap/footer.php
+++ wp-content/themes/understrap/footer.php
@@ -15,31 +15,125 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
 
-<div class="wrapper" id="wrapper-footer">
+<footer class="py-5 cite">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-6">
+          <h3>Menù</h3>
+          <ul class="m-0 p-0">
+            <li class="py-1">
+              <a href="#">IL LIBRO</a>
+            </li>
 
-	<div class="<?php echo esc_attr( $container ); ?>">
+            <li class="py-1">
+              <a class="crwd" href="#">IL CROWDFUNDING</a>
+              <ul>
+                <li>
+				  <hr>
+                  <a href="#">COME FUNZIONA</a>
+                  <hr>
+                </li>
+                <li>
+                  <a class="crwd" href="#">SOSTIENI IL NOSTRO PROGETTO SU PRODUZIONI DAL BASSO</a>
+                  <hr>
+                </li>
+                <li>
+                  <a href="#">LA PAGINA DEI RINGRAZIAMENTI</a>
+                  <hr>
+                </li>
+              </ul>
+            </li>
 
-		<div class="row">
+            <li class="py-1">
+              <a href="#">L'AUTORE</a>
+              <ul>
+                <li>
+				<hr>
+                  <a href="#">SULLA CARTA</a>
+                  <hr>
+                </li>
+                <li>
+                  <a href="#">SULLO SCHERMO</a>
+                  <hr>
+                </li>
+              </ul>
+            </li>
 
-			<div class="col-md-12">
+            <li class="py-1">
+              <a href="#">BLOG</a>
+            </li>
 
-				<footer class="site-footer" id="colophon">
+            <li class="py-1">
+              <a href="#">CONTATTI</a>
+              <ul>
+                <li>
+				<hr>
+                  <a href="#">PER I CURIOSI</a>
+                  <hr>
+                </li>
+                <li>
+                  <a href="#">PER LA STAMPA</a>
+                  <hr>
+                </li>
+              </ul>
+            </li>
 
-					<div class="site-info">
+          </ul>
+        </div>
+        <div class="col-12 col-sm-6">
+          <h3>Sostieni il nostro progetto</h3>
+          <a class="crwd" href="#">SCOPRI COME FARE</a>
 
-						<?php understrap_site_info(); ?>
+          <h3 class="mt-5">Mail</h3>
+			<?php
+			// check if the repeater field has rows of data
+			if( have_rows('mailing_list') ):
+				// loop through the rows of data
+				while ( have_rows('mailing_list') ) : the_row();
+					// display a sub field value
+					?>
 
-					</div><!-- .site-info -->
+					<?php 
+					$link = get_sub_field('mail');
+					if( $link ): 
+						$link_url = $link['url'];
+						$link_title = $link['title'];
+						$link_target = $link['target'] ? $link['target'] : '_self';
+						?>
+						<a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+						<?php echo '<br>'; ?>
+				
+					<?php endif; ?>
 
-				</footer><!-- #colophon -->
+					<?php
+				endwhile;
+			else :
+				// no rows found
+			endif;
+			?>
 
-			</div><!--col end -->
-
-		</div><!-- row end -->
-
-	</div><!-- container end -->
-
-</div><!-- wrapper end -->
+          <h3 class="mt-5">Social</h3>
+          <ul class="p-0 m-0 list-inline">
+            <li class="list-inline-item">
+              <a href="https://www.facebook.com/Fabianolioi/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://twitter.com/FabianoLioi" target="_blank"><i class="fab fa-twitter"></i></a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.instagram.com/fabianolioi/" target="_blank"><i class="fab fa-instagram"></i></a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://vimeo.com/fabianolioi" target="_blank"><i class="fab fa-vimeo"></i></a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.youtube.com/user/MrFabianoLioi" target="_blank"><i class="fab fa-youtube"></i></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
 
 </div><!-- #page we need this extra closing tag here -->
 
