f0bb0153a311615f0c6a25194395ba9c59a5dde5
Francesco Matacotti <francesco1984.go@gmail.com>
2020-02-09 18:29:28 +0100

    disabilitato alcune voci del menu e impostato pagina COMING SOON

---
+++ wp-content/themes/understrap/page-templates/coming-soon.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template Name: Coming soon
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<?php if ( is_front_page() ) : ?>
+  <?php get_template_part( 'global-templates/hero' ); ?>
+<?php endif; ?>
+
+
+<div class="d-flex justify-content-center align-items-center comingsoon">
+
+<h1>coming soon</h1>
+  
+</div>
+ 
+<hr>
+
+<?php get_footer(); ?>
