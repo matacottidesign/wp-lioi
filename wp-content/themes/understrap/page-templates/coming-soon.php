70930111159a79b569cd0091634e91bc60118663
Francesco Matacotti <francesco1984.go@gmail.com>
2020-02-09 18:53:54 +0100

    aggiunto testo in COMING SOON

--- wp-content/themes/understrap/page-templates/coming-soon.php
+++ wp-content/themes/understrap/page-templates/coming-soon.php
@@ -19,9 +19,12 @@ $container = get_theme_mod( 'understrap_container_type' );
 <?php endif; ?>
 
 
-<div class="d-flex justify-content-center align-items-center comingsoon">
+<div class="d-flex align-items-center text-center comingsoon">
 
-<h1>coming soon</h1>
+<div class="container">
+<h1 class="mb-5">coming soon</h1>
+<p>La nostra campagna di Crowdfunding per stampare O.I. L’arte in una frattura e realizzare la mostra con le opere ancora non è iniziata, ma di certo non ti lasciamo all’oscuro di tutto! <br> Seguici sulla nostra pagina Facebook per avere tutti gli aggiornamenti, e aiutaci a realizzare questo sogno. Insieme è più bello!</p>
+</div>
   
 </div>
  
